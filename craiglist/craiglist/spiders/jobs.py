2b1438b14505cda1c7a80b1822f97a16df11db57
congthanhnguyen <te304584>
2018-06-11 10:42:28 +0900

    update section16

---
+++ craiglist/craiglist/spiders/jobs.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class JobsSpider(scrapy.Spider):
+    name = 'jobs'
+    allowed_domains = ['newyork.craigslist.org']
+    start_urls = ['https://newyork.craigslist.org/search/egr']
+
+    def parse(self, response):
+        listings = response.xpath('//a[@class="result-title hdrlnk"]/text()').extract()
+
+        for listing in listings:
+            yield {'listing':listing}
